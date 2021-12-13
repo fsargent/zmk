501b28a2e146ae830bd7d0c9cf72db532e74e29c
Felix Sargent <felix@truework.com>
2021-12-13 11:03:16 -0800

    Personal updates.

---
+++ app/boards/shields/boardsource5x12/combos.c
@@ -0,0 +1,63 @@
+
+	combo_lbrc {
+		timeout-ms=<25>;
+		key-positions=<24 25>;
+		bindings=<&kp LBRC>;
+	};
+	
+	combo_lt {
+		timeout-ms=<25>;
+		key-positions=<25 26>;
+		bindings=<&kp LT>;
+	};
+	
+	combo_lpar {
+		timeout-ms=<25>;
+		key-positions=<26 27>;
+		bindings=<&kp LPAR>;
+	};
+	
+	combo_lbkt {
+		timeout-ms=<25>;
+		key-positions=<27 28>;
+		bindings=<&kp LBKT>;
+	};
+
+
+	combo_rbkt {
+		timeout-ms=<25>;
+		key-positions=<31 32>;
+		bindings=<&kp RBKT>;
+	};
+	combo_rpar {
+		timeout-ms=<25>;f 
+		key-positions=<32 33>;
+		bindings=<&kp RPAR>;
+	};
+	combo_rt {
+		timeout-ms=<25>;
+		key-positions=<33 34>;
+		bindings=<&kp GT>;
+	};
+	combo_rbrc {
+		timeout-ms=<25>;
+		key-positions=<34 35>;
+		bindings=<&kp RBRC>;
+	};
+
+	combo_grav{
+		timeout-ms=<25>;
+		key-positions=<25 37>;
+		bindings=<&kp GRAV>;
+	};
+
+	combo_minus{
+		timeout-ms=<25>;
+		key-positions=<44 45>;
+		bindings=<&kp MINUS>;
+	};
+	combo_eql{
+		timeout-ms=<25>;
+		key-positions=<45 46>;
+		bindings=<&kp EQL>;
+	};
